db4a7467ef96c38fc1b4d0c9e0478b7899499889
Nicole Dow <nikki.dow71@gmail.com>
2019-05-08 21:06:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:\n"
+    puts "#{season.capitalize}:"
     holiday.each do |holiday, item|
       puts "  #{holiday.capitalize}:" + item.join(", ")
     end
