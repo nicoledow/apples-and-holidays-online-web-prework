75351a1f1affd7551273279d5aec6c2d581167e4
Nicole Dow <nikki.dow71@gmail.com>
2019-05-08 21:17:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, item|
-      puts "  #{holiday.capitalize}:" + " " + item.join(", ")
+      puts "  #{holiday.to_s.split("_").map(&:capitalize).join(" ")}:" + " " + item.join(", ")
     end
   end
 end
